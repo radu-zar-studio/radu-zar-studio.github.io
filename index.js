199af5c69a92a551c94f3afa271a121210f95c80
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2023-06-03 14:42:38 +0000

    Small scroll fix.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 document.getElementById("mobilenav").addEventListener("click", () => {
   document.body.classList.add("mobilenav-visible");
-  window.scrollTo(document.body.scrollWidth, 0);
+  document.querySelector("body>nav").scrollIntoView();
 });
 
 document.getElementById("closenav").addEventListener("click", () => {
   document.body.classList.remove("mobilenav-visible");
-  window.scrollTo(0, 0);
+  document.querySelector("body>main").scrollIntoView();
 });
 
 document.body.classList.remove("mobilenav-visible");
-window.scrollTo(0, 0);
+document.querySelector("body>main").scrollIntoView();
