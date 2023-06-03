e8724bc77c5dbc0a645f495ad6e3db87ea6c45d4
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2023-06-03 20:13:32 +0000

    Better mobile nav handling.

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 document.getElementById("mobilenav").addEventListener("click", () => {
-  document.body.classList.add("mobilenav-visible");
-  document.querySelector("body>nav").scrollIntoView();
-});
-
-document.getElementById("closenav").addEventListener("click", () => {
-  document.body.classList.remove("mobilenav-visible");
-  document.querySelector("body>main").scrollIntoView();
+  document.body.classList.toggle("mobilenav-visible");
 });
 
 document.body.classList.remove("mobilenav-visible");
-document.querySelector("body>main").scrollIntoView();
